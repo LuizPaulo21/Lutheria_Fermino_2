f584ed9fd8aaf1e2d2789fa10c15a068b7edd24a
Luiz Paulo <luiz.paulo.neto@hotmail.com>
2025-04-25 18:55:27 -0300

    Validação de dados da entidade cliente

--- Flask/entidades/cliente.py
+++ Flask/entidades/cliente.py
@@ -1,4 +1,6 @@
 #Classe cliente com seu construtor e dados necessários
+import re
+
 class Cliente:
     
     def __init__(self, nome, cpf, cep, endereco, complemento, email, telefone, celular):
@@ -11,3 +13,19 @@ class Cliente:
         self.telefone = telefone,
         self.celular = celular
 
+    def __post_init__(self):
+        # Valida o CPF do cliente
+        if not self._validar_cpf(self.cpf):
+            raise ValueError("CPF inválido")
+        if not self.validar_email(self.email):
+             raise ValueError("Email inválido")
+
+    def validar_email(self, email: str) -> bool:
+        # Validação do email
+        pattern = r"^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$"
+        return bool(re.match(pattern, email))
+    
+    def _validar_cpf(self, cpf: str) -> bool:
+        # Implementar lógica de validação de CPF (simplificada aqui)
+        cpf_numerico = re.sub(r'[^0-9]', '', cpf)
+        return len(cpf_numerico) == 11 # Exemplo muito básico
